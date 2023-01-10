55c0fbeee803db3111a62bf5d86b3cfe0dc7637c
gurubani <gurubani@outlook.in>
2023-01-10 18:58:36 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# gurubani.github.io
--- gatsby-browser.js
+++
@@ -1,4 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "@popperjs/core/dist/umd/popper.min.js";
-import "highlight.js/styles/atom-one-dark-reasonable.css";
